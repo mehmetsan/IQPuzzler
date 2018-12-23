67569840d052b4715b2f872ddad0fd6e3bd125a4
GameOfObjects <43797018+GameOfObjects@users.noreply.github.com>
2018-12-23 20:32:10 +0300

    Update

--- IQ Puzzler/src/CreditsPanel.java
+++ IQ Puzzler/src/CreditsPanel.java
@@ -1,45 +1,40 @@
-/*
-* IQ Puzzler Term Project
-* CS319  Section:2
-* This project was developed for Object Oriented Programming Course Term Project by
-* -Arda Türkoğlu
-* -Engin Deniz Kopan
-* -Zafer Çınar
-* -Mehmet Selim Özcan
-* -Mehmet Sanisoğlu
-*/
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.SwingConstants;
-
-
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class CreditsPanel extends JPanel {
 
 	private GameOfObjectsMenu menu;
+
 	private static final long serialVersionUID = 1L;
 
-	/*
-	 * Constructor of CreditsPanel Class
-	 */
+	/**
+	 * Create the panel.*/
 	public CreditsPanel(GameOfObjectsMenu x) {
 		setLayout(null);
 		setBackground(new Color(8,178 ,227));
 		menu =x;
 		JLabel label = new JLabel("Credits");
-		label.setFont(new Font("Tahoma", Font.BOLD, 35));
+		//label.setHorizontalAlignment(SwingConstants.CENTER);
+
+		label.setFont(new Font("Tahoma", Font.BOLD, 40));
+		label.setForeground(Color.orange);
 		label.setBounds(20, 25, 240, 40);
 		add(label);
+
 		JButton btnNewButton = new JButton("Back");
+		//btnNewButton.setHorizontalAlignment(SwingConstants.CENTER);
 		btnNewButton.setBounds(20, 500, 200, 60);
 		btnNewButton.setBackground(new Color(59, 89, 182));
 		btnNewButton.setForeground(Color.WHITE);
@@ -47,30 +42,40 @@ public class CreditsPanel extends JPanel {
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 25));
 		btnNewButton.addActionListener(new backListener());
 		add(btnNewButton);
+
 		JLabel labels[] = new JLabel[5];
+		//label2.setHorizontalAlignment(SwingConstants.CENTER);
 		labels[0] = new JLabel("Arda Türkoğlu: arda.turkoglu@ug.bilkent.edu.tr");
 		labels[1] = new JLabel("Engin Deniz Kopan: engin.kopan@ug.bilkent.edu.tr");
 		labels[2] = new JLabel("Mehmet Sanisoğlu: mehmet.sanisoglu@ug.bilkent.edu.tr");
 		labels[3] = new JLabel("Selim Özcan: selim.ozcan@ug.bilkent.edu.tr");
 		labels[4] = new JLabel("Zafer Çınar: zafer.cinar@ug.bilkent.edu.tr");
 		for( int i = 0; i < 5; i++ ) {
-			labels[i].setFont(new Font("Tahoma", Font.PLAIN, 17));
-			labels[i].setBounds(20, 100 + (i * 20), 500, 30);
+			labels[i].setFont(new Font("Tahoma", Font.BOLD, 25));
+			labels[i].setForeground(Color.orange);
+			labels[i].setBounds(20, 100 + (i * 40), 700, 120);
 			add(labels[i]);
 		}
-		
+
 	}
-	/*
-	* This listener class is added to button in credits panel to
-	* come back to main menu.
-	*/
+
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+		ImageIcon img;
+		img = new ImageIcon("background.jpg");
+		img.paintIcon(null, g, 0, 0);
+	}
+
 	public class backListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 
 			setVisible(false);
 			MainPanel m = new MainPanel(menu);
 			menu.show(m);
+
 		}	
-		
-	} 
+
+	}
+
 }
